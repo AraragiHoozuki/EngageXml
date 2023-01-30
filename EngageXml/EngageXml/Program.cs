b94aaa114f35735df44a7fd699906b189a04d070
Georges Zebit <araragi.hoozuki@yandex.ru>
2023-01-30 19:48:23 +0800

    Add files via upload

--- EngageXml/EngageXml/Program.cs
+++ EngageXml/EngageXml/Program.cs
@@ -1,37 +1,83 @@
-﻿using OfficeOpenXml;
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace EngageXml
 {
     class Program
     {
+        static AssetsManager AM;
         static void Main(string[] args)
         {
-            string path = args[0];
-            if (!File.Exists(path)) throw new Exception("Error: File not found!"); 
-            if (path.EndsWith(".xml"))
+            string arg1 = args[0];
+            if (arg1.StartsWith("-"))
             {
-                Xml2Xlsx(path);
-            } else if (path.EndsWith(".xlsx"))
-            {
-                Xlsx2Xml(path);
+                AM = new AssetsManager();
+                switch(arg1) {
+                    case "-in":
+                        string dataPath = args[1];
+                        byte[] data = dataPath.EndsWith(".xlsx") ? Xlsx2Xml(dataPath): File.ReadAllBytes(dataPath);
+                        InsertAsset(data, args[2]);
+                        break;
+                    case "-out":
+                        if (args[1] == "-xlsx")
+                        {
+                            ExtractAsset(args[2], true);
+                        } else
+                        {
+                            ExtractAsset(args[1], false);
+                        }
+                        break;
+                    default:
+                        throw new Exception($"Invalid option: {arg1}");
+                }
+
             } else
             {
-                new Exception("Error: File format not supported!");
+                // conversion between xml and xlsx
+                string path = arg1;
+                if (!File.Exists(path)) throw new Exception("Error: File not found!");
+                if (path.EndsWith(".xml"))
+                {
+                    File.WriteAllBytes(path + ".xlsx", Xml2Xlsx(path));
+                    
+                }
+                else if (path.EndsWith(".xlsx"))
+                {
+                    File.WriteAllBytes(Path.ChangeExtension(path, null), Xlsx2Xml(path));
+                }
+                else
+                {
+                    new Exception("Error: File format not supported!");
+                }
             }
+
         }
 
+        static byte[] Xml2Xlsx(byte[] data)
+        {
+            XmlDocument xml = new XmlDocument();
+            string xmlText = Encoding.UTF8.GetString(data);
+            xmlText = xmlText.Substring(1, xmlText.Length - 1);
+            xml.LoadXml(xmlText);
+            return Xml2XlsxReal(xml);
+        }
 
-        static void Xml2Xlsx(string path)
+        static byte[] Xml2Xlsx(string path)
         {
             XmlDocument xml = new XmlDocument();
             xml.Load(path);
+            return Xml2XlsxReal(xml);
+        }
+
+        static byte[] Xml2XlsxReal(XmlDocument xml)
+        {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             ExcelPackage pkg = new ExcelPackage();
             XmlNode bookNode = xml.DocumentElement.SelectSingleNode("/Book");
@@ -86,17 +132,17 @@ namespace EngageXml
             }
 
 
-
-            FileStream fs = File.Create($"{path}.xlsx");
-            fs.Close();
-            File.WriteAllBytes($"{path}.xlsx", pkg.GetAsByteArray());
+            byte[] xlsxBytes = pkg.GetAsByteArray();
             pkg.Dispose();
+            return xlsxBytes;
         }
 
-        static void Xlsx2Xml(string path)
+        static byte[] Xlsx2Xml(string path)
         {
             FileInfo file = new FileInfo(path);
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            byte[] xmlBytes;
+
             using ( ExcelPackage pkg = new ExcelPackage(file))
             {
                 var sheets = pkg.Workbook.Worksheets
@@ -162,10 +208,86 @@ namespace EngageXml
                             nodeParam.SetAttribute(paramAttrs[col - 1], sheet.Cells[row, col].Text);
                         }
                     }
+                }
 
-                    xml.Save(Path.ChangeExtension(path, null));
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    xml.Save(ms);
+                    xmlBytes = ms.ToArray();
                 }
             }
+
+            return xmlBytes;
+        }
+
+        static void InsertAsset(byte[] data, string bundlePath)
+        {
+            var bun = AM.LoadBundleFile(bundlePath);
+
+            //load first asset from bundle
+            var inst = AM.LoadAssetsFileFromBundle(bun, 0);
+            if (!inst.file.typeTree.hasTypeTree)
+                AM.LoadClassDatabaseFromPackage(inst.file.typeTree.unityVersion);
+            var inf = inst.table.assetFileInfo[0].index == 1 ? inst.table.assetFileInfo[1] : inst.table.assetFileInfo[0];
+            var baseField = AM.GetTypeInstance(inst, inf).GetBaseField();
+            baseField.Get("m_Script").GetValue().Set(data);
+
+            var newGoBytes = baseField.WriteToByteArray();
+            var repl = new AssetsReplacerFromMemory(0, inf.index, (int)inf.curFileType, 0xffff, newGoBytes);
+
+            //write changes to memory
+            byte[] newAssetData;
+            using (var stream = new MemoryStream())
+            using (var writer = new AssetsFileWriter(stream))
+            {
+                inst.file.Write(writer, 0, new List<AssetsReplacer>() { repl }, 0);
+                newAssetData = stream.ToArray();
+            }
+
+            //rename this asset name from boring to cool when saving
+            var bunRepl = new BundleReplacerFromMemory(inst.name, null, true, newAssetData, -1);
+            byte[] newBundleData;
+            using (var stream = new MemoryStream())
+            using (var bunWriter = new AssetsFileWriter(stream))
+            {
+                bun.file.Write(bunWriter, new List<BundleReplacer>() { bunRepl });
+                newBundleData = stream.ToArray();
+            }
+
+
+            MemoryStream newBundleStream = new MemoryStream(newBundleData);
+            bun = AM.LoadBundleFile(newBundleStream, $"{bundlePath}.mod");
+            AM.UnloadBundleFile(bundlePath);
+
+            using (var stream = File.OpenWrite(bundlePath))
+            using (var writer = new AssetsFileWriter(stream))
+            {
+                bun.file.Pack(bun.file.reader, writer, AssetBundleCompressionType.LZ4);
+            }
+
+            AM.UnloadAllBundleFiles();
+        }
+
+        static void ExtractAsset(string bundlePath, bool toXlsx) 
+        {
+            var bun = AM.LoadBundleFile(bundlePath);
+
+            //load first asset from bundle
+            var inst = AM.LoadAssetsFileFromBundle(bun, 0);
+            if (!inst.file.typeTree.hasTypeTree)
+                AM.LoadClassDatabaseFromPackage(inst.file.typeTree.unityVersion);
+            var inf = inst.table.assetFileInfo[0].index == 1 ? inst.table.assetFileInfo[1] : inst.table.assetFileInfo[0];
+            var baseField = AM.GetTypeInstance(inst, inf).GetBaseField();
+            byte[] data = baseField.Get("m_Script").GetValue().AsStringBytes();
+
+            if (!toXlsx)
+            {
+                File.WriteAllBytes(Path.ChangeExtension(bundlePath, null), data);
+            } else
+            {
+                File.WriteAllBytes(Path.ChangeExtension(bundlePath, ".xlsx"), Xml2Xlsx(data));
+            }
+
         }
     }
 }
